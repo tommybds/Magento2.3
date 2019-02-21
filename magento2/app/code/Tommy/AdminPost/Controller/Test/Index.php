d231f36b2da20ce272dae520a033568750f7a4d1
Tommy Bordas <tommy.bordas@gmail.com>
2019-02-21 15:43:51 +0100

    Création controller

---
+++ magento2/app/code/Tommy/AdminPost/Controller/Test/Index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Tommy\AdminPost\Controller\Test;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        die('test index');
+    }
+}
